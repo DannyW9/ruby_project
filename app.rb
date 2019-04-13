5f07c9a098f6b4857c171147bfd9cad1ae9b0c90
Danny Welsh <dannywelsh400@hotmail.com>
2019-04-13 19:58:38 +0100

    creates initial splash index page

---
+++ app.rb
@@ -0,0 +1,10 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('controllers/members_controller.rb')
+require_relative('controllers/gym_classes_controller.rb')
+require_relative('controllers/reservations_controller.rb')
+require_relative('controllers/sessions_controller.rb')
+
+get '/' do
+  erb(:index)
+end
